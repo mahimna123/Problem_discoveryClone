0f511110c5d14a3179d91ea1117051a5378bd5d1
mahimna123 <joshimahimna@gmail.com>
2025-02-18 11:04:40 +0530

    Updated app.js with MongoDB connection changes

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ const reviewRoutes = require('./routes/reviews');
 const MongoDBStore = require("connect-mongo")(session);
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection; 
